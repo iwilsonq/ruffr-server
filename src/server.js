0a858f54a16baf901d62e76ed2f4283fbe694e6f
Ian Wilson <iwilsonq@gmail.com>
2016-12-26 23:23:30 -0800

    updated whitelist

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ mongoose.Promise = Promise;
 
 mongoose.connect('mongodb://heroku_wk142pt6:1l9d6vqn8sh9s6lnq3hvvnmihb@ds141088.mlab.com:41088/heroku_wk142pt6');
 
-const whitelist = ['http://localhost:3000', 'https://aqueous-dusk-57308.herokuapp.com'];
+const whitelist = ['http://localhost:1337', 'https://ruffr.herokuapp.com'];
 
 const corsOptions = {
   origin: function(origin, callback){
