5d489c99ccd9782a451056b8153b9900f06dc567
Ian Wilson <iwilsonq@gmail.com>
2016-12-27 18:52:18 -0800

    committing config

---
+++ config.js
@@ -0,0 +1,16 @@
+const cloudConfig = {
+  cloud_name:	'ruffr',
+  api_key: '523783848114792',
+  api_secret:	'NbwOB2G9tYRb4XtO7P-L27hHWPY'
+};
+
+const dbCredentials = {
+  user: 'heroku_wk142pt6',
+  pass: '1l9d6vqn8sh9s6lnq3hvvnmihb'
+};
+
+
+module.exports = {
+  cloudConfig,
+  dbCredentials
+};
